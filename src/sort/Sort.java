82a69034e1b076e8463adce320953b8e4ad50c45
Gilmour <horriganb@gilmour.org>
2015-11-19 14:28:34 -0600

    Almost done

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -13,39 +13,40 @@ import java.util.Scanner;
  * @author gilmour
  */
 public class Sort {
-
+    List<Integer> ints = new ArrayList<Integer>();
+    Scanner in = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<Integer> ints = new ArrayList<Integer>();
-        Scanner in = new Scanner(System.in);
-    }
-
-    public void histoInt() {
-
-        int counter;
-
-        for (int k = 0; k <= 50; k++) {
-
-            System.out.print(k + ": ");
-
-            counter = 0;
-
-            for (Integer x : ints) {
-
-                if (x.equals(k)) {
-
-                    counter++;
-
-                }
-
+        public void Sorts(){
+            System.out.println("Let's build our list.");
+            System.out.println("Enter a number from 0 - 50.");
+            System.out.println("Enter a # outside the range to exit input phase.");
+            int input = in.nextInt();
+            while (input <= 50 && input >= 0){
+                ints.add(input);
+                input = in.nextInt();
             }
-
-            System.out.print(counter + "\n");
-
         }
-
+    public void histoInt(){
+        int counter;
+        for(int k = 0; k <= 50; k++){
+            System.out.print(k + ": ");
+            counter = 0;
+            for (Integer x : ints){
+                if (x.equals(k)){
+                    counter++;
+                }
+            }
+        System.out.print(counter + "\n");
+        }
     }
-
 }
+
+
+        
+  
+
+    
+
